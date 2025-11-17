f0a1ac6195459df7ea2c54f02096d240dc27030c
Ngoc Linh Pham <ngoclinhphvm@gmail.com>
2025-11-17 18:29:19 +0700

    fix: delete create user endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,38 +1,13 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  ConflictException,
-} from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiOkResponse, ApiCreatedResponse } from '@nestjs/swagger';
+import { ApiOkResponse } from '@nestjs/swagger';
 import { User } from './entities/user.entity';
 import { ParseUUIDPipe } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @ApiCreatedResponse({ type: User })
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    const { username, email } = createUserDto;
-    const existingUsername = await this.usersService.findOne({ username });
-    if (existingUsername) {
-      throw new ConflictException('User with this username already existed');
-    }
-    const existingEmail = await this.usersService.findOne({ email });
-    if (existingEmail) {
-      throw new ConflictException('User with this email already existed');
-    }
-    return this.usersService.create(createUserDto);
-  }
 
   @Get()
   @ApiOkResponse({ type: User, isArray: true })
